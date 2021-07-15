ef31d6e750e5997ace34a314980d8c063651f372
Rebaz Saleh <rebaz88.software@gmail.com>
2021-07-15 07:28:39 +0300

    replace filter with find

--- src/blockchain.js
+++ src/blockchain.js
@@ -161,7 +161,7 @@ class Blockchain {
     getBlockByHash(hash) {
         let self = this;
         return new Promise((resolve, reject) => {
-            let block = self.chain.filter(p => p.hash === hash)[0];
+            let block = self.chain.find(p => p.hash === hash);
             if (block) {
                 resolve(block);
             } else {
@@ -178,7 +178,7 @@ class Blockchain {
     getBlockByHeight(height) {
         let self = this;
         return new Promise((resolve, reject) => {
-            let block = self.chain.filter(p => p.height === height)[0];
+            let block = self.chain.find(p => p.height === height);
             if (block) {
                 resolve(block);
             } else {
